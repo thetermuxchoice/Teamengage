23fb024dadae7f7937a10280ce7a8e4b1f8014ee
Gmacri99 <macrigianny2021@gmail.com>
2023-06-09 21:36:01 -0400

    cambios del contenedor about

--- Js.js
+++ Js.js
@@ -2,6 +2,7 @@ const $btnClose=document.getElementById('btn-close')
 const $btnModal=document.getElementById('btn-modal')
 const $btnModal2=document.getElementById('btn-modal2')
 const $btnModal3=document.getElementById('btn-modal3')
+const $btnModal5=document.getElementById('btn-modal5')
 const $btnModal4=document.getElementById('btn-modal4')
 const $item=document.getElementById('item')
 const $modal=document.getElementById('div-modal')
@@ -20,6 +21,13 @@ $btnClose.addEventListener("click",(e)=>{
 /*Botones para abrir la modal */
 
 $btnModal.addEventListener("click",(e)=>{
+    e.preventDefault()
+    $modal.style.display='block'
+    
+})
+
+
+$btnModal5.addEventListener("click",(e)=>{
     e.preventDefault()
     $modal.style.display='block'
     
